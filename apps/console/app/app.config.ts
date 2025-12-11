4c1c615ec26489dc6f6876baa3e1782ffe4e659e
Saddam Azad <saddam.azad@me.com>
2025-12-12 01:22:34 +0600

    customRuntime bun fix

--- apps/console/app/app.config.ts
+++ apps/console/app/app.config.ts
@@ -57,8 +57,8 @@ export default defineAppConfig({
     },
   ],
   stacks: [
-    { type: "SPA", source: "@thunderso/cdk-spa", version: "0.23.9" },
-    { type: "FUNCTION", source: "@thunderso/cdk-functions", version: "0.7.8" },
+    { type: "SPA", source: "@thunderso/cdk-spa", version: "0.23.12" },
+    { type: "FUNCTION", source: "@thunderso/cdk-functions", version: "0.7.9" },
     { type: "WEB_SERVICE", source: "@thunderso/cdk-webservice", version: "0.3.1" },
   ],
   fargate: {
